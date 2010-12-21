37fbe48a6da4b1cdfecec4ff18007c3fc3fbf495
unknown <abrady@.Us.Atari.loc>
2010-12-20 19:23:59 -0800

    first wsgi app

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,3 +1,15 @@
-print 'Content-type: text/plain'
-print ''
-print 'Hello, world!'
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.out.write('Hello, webapp World!')
+
+app = webapp.WSGIApplication([('/',MainPage)], debug=True)
+
+def main():
+	run_wsgi_app(app)
+
+if __name__ == "__main__":
+	main()
