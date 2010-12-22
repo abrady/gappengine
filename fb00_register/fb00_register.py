c1c2c4382e127ea8be6874f317fd6a2e2b781225
unknown <abrady@.Us.Atari.loc>
2010-12-22 11:40:37 -0800

    cleanup app.yaml. fb register

--- fb00_register/fb00_register.py
+++ fb00_register/fb00_register.py
@@ -2,9 +2,9 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class LoginPage(webapp.RequestHandler):
+class RegisterPage(webapp.RequestHandler):
 	def get(self):
-		fb_iframe = """<iframe src='http://www.facebook.com/plugins/registration.php?client_id=166075663433750&redirect_uri=http%3A%2F%2Fdrinkbot9000.appspot.com%2Ffb00_login%2Flogged_in%2F&
+		fb_iframe = """<iframe src='http://www.facebook.com/plugins/registration.php?client_id=166075663433750&redirect_uri=http%3A%2F%2Fdrinkbot9000.appspot.com%2Ffb00_register%2Fregistered&
 		fields=name,birthday,gender,location,email'
         scrolling='auto'
         frameborder='no'
@@ -14,21 +14,26 @@ class LoginPage(webapp.RequestHandler):
         height='330'>
 		</iframe>"""		
 		self.response.headers['Content-Type'] = 'text/html'
-		self.response.out.write('fb login frame<br/>')
+		self.response.out.write('fb frame for registering:')
 		self.response.out.write(fb_iframe)
+		self.response.out.write('query string: ' + self.request.query_string + '<br/>')
+		self.response.out.write('path: ' + self.request.path + '<br/>')
 
-class LoggedInPage(webapp.RequestHandler):
+
+class RegisteredPage(webapp.RequestHandler):
 	def get(self):
 			self.response.headers['Content-Type'] = 'text/html'
-			self.response.out.write('Logged in to facebook!<br/>')
+			self.response.out.write('<head></head><body>')
+			self.response.out.write('registered via facebook!<br/>')
 			self.response.out.write('query string: ' + self.request.query_string + '<br/>')
 			self.response.out.write('path: ' + self.request.path + '<br/>')
+			self.response.out.write('</body>')			
 		
 
 app = webapp.WSGIApplication(
 	[
-	('/.*logged_in',LoggedInPage),
-	('/.*',LoginPage)
+	('/.*registered',RegisteredPage),
+	('/.*',RegisterPage)
 	], debug=True)
 
 def main():
